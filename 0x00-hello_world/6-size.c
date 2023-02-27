6f71e0c580059baeca4ca75d56a3dd2bd02971f1
Muhammed101Ahmed <mohammeddahmed98@gmail.com>
2023-02-27 05:52:17 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,5 @@ printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
-return 0 (Success);
+return main(0);
 }
