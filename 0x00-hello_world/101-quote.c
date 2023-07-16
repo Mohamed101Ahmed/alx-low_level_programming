c73cac885cf36bb513a617c196a29491aa442e58
Mohamed101Ahmed <mohammeddahmed98@gmail.com>
2023-07-16 07:43:42 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline to standard error.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * followed by a newline, to standard error
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
