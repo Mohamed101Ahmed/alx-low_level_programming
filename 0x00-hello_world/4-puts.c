7e3def7e5f63404330d3e0504717ef2abbe30200
Mohamed101Ahmed <mohammeddahmed98@gmail.com>
2023-07-16 07:39:44 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main - A program that prints a string and new line '/' with puts function
+ * You are not allowed to use printf
+ * Your program should end with the value 0
  * Return: 0 (success)
  */
 int main(void)
